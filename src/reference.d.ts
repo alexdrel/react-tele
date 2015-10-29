c916f3325c0b60848b1f206e9d7de478f03cb779
Vlad Yanchevsky <yanchevsky.v@gmail.com>
2015-10-29 18:29:26 +0200

    update slide-nav, lint fixes

--- src/reference.d.ts
+++ src/reference.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../typings/react/react.d.ts" />
 /// <reference path="../typings/es6-promise/es6-promise.d.ts" />
+/// <reference path="../typings/classnames/classnames.d.ts" />
